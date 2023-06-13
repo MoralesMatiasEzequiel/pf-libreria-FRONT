ceabceba33624b58f11e25aadd04924c96c400ad
Sofía Chiabudini <sofiachiabudini@gmail.com>
2023-06-13 17:51:13 -0300

    CONDITIONAL RENDER OF MENU COMPONENT

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import Nav from './components/common/Nav/Nav';
 import Menu from './components/common/Menu/Menu';
 import Footer from './components/common/Footer/Footer';
@@ -8,16 +8,23 @@ import About from './components/views/About/About'
 import Shop from './components/views/Shop/Shop'
 
 function App() {
+
+  const location = useLocation();
+
+  //acá defino en qué rutas no se verá el menú de categorías, son de prueba estas
+  const hideMenu = location.pathname.includes('/cart') || location.pathname.includes('/payment');
+
   return (
     <div className="App">
       <Nav />
-      <Menu />
+      {!hideMenu && <Menu />}
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
-        <Route path='/about' element={<About />} />
         <Route path='/shop' element={<Shop />} />
         <Route path='/product/:id' />
+        <Route path='/cart' />
+        <Route path='/payment' />
       </Routes>
       <Footer />
     </div>
