524e078b8a356ac0519ca561f72a3622975f4e4e
diegoandres22 <diego.a.v3005@gmail.com>
2023-06-14 19:05:43 -0300

    instalo boostrap y ajusto nav1

--- src/components/common/Nav/Nav.jsx
+++ src/components/common/Nav/Nav.jsx
@@ -1,33 +1,35 @@
-import { NavLink} from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import style from "./Nav.module.css"
 
 
 const Nav = () => {
 
-    return(
+    return (
+
         <nav className={style.nav}>
             <div className={style.div}>
                 <NavLink to={"/"}>
-                    <img src="" alt="logo" />
+                    <img className={style.logo} src="https://images.emojiterra.com/google/noto-emoji/unicode-15/color/512px/1f4da.png" alt="lilbrito" />
                 </NavLink>
             </div>
 
-            <div className={style.div}>
+            <div className={style.div2}>
                 <ul className={style.ul}>
-                    <li>
+                    <li className={style.li}>
                         <NavLink to={"/about"} className={style.navLink}>Contacto</NavLink>
                     </li>
 
-                    <li>
+                    <li className={style.li} >
                         <NavLink to={""} className={style.navLink}>Ingresar / Registrarse</NavLink>
                     </li>
 
-                    <li>
+                    <li className={style.li} >
                         <NavLink to={"/cart"} className={style.navLink}>Carrito</NavLink>
                     </li>
                 </ul>
             </div>
         </nav>
+
     )
 
 }
