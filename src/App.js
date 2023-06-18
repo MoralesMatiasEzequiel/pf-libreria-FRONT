7b26a79ff96aedfcb61a48c0b8ddb60538acbddc
GJRC23 <gastuconsoli8@gmail.com>
2023-06-18 15:50:52 -0300

    footer y boton arrepentimiento bien hecho

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
+import { Suspense, lazy } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
-import Navuno from './components/common/Nav/Nav';
-import Menu from './components/common/Menu/Menu';
-import Footer from './components/common/Footer/Footer';
-import Home from './components/views/Home/Home';
-import About from './components/views/About/About';
-import Shop from './components/views/Shop/Shop';
-import Detail from './components/views/Shop/Detail/Detail';
-import Arrepentimiento from './components/views/Arrepentimiento/Arrepentimiento';
+
+const Navuno = lazy(() => import('./components/common/Nav/Nav'));
+const Menu = lazy(() => import('./components/common/Menu/Menu'));  
+const Footer = lazy(() => import('./components/common/Footer/Footer'));  
+const Home = lazy(() => import('./components/views/Home/Home'));  
+const About = lazy(() => import('./components/views/About/About'));  
+const Shop = lazy(() => import('./components/views/Shop/Shop'));  
+const Detail = lazy(() => import('./components/views/Shop/Detail/Detail'));  
+const Arrepentimiento = lazy(() => import('./components/views/Arrepentimiento/Arrepentimiento'));
 
 function App() {
 
@@ -18,20 +20,24 @@ function App() {
 
   return (
     <div className="App">
-      <Navuno />
-      {!hideMenu && <Menu />}
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
-        <Route path='/shop' element={<Shop />} />
-        <Route path='/shop/:id' element={<Detail />} />
-        <Route path='/cart' />
-        <Route path='/payment' />
-        <Route path="/arrepentimiento" element={<Arrepentimiento />} />
-      </Routes>
-      <Footer />
+      <Suspense fallback={<h1>Cargando ...</h1>}>
+        <Navuno />
+        {!hideMenu && <Menu />}
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/shop' element={<Shop />} />
+          <Route path='/shop/:id' element={<Detail />} />
+          <Route path='/cart' />
+          <Route path='/payment' />
+          <Route path="/arrepentimiento" element={<Arrepentimiento />} />
+        </Routes>
+        <Footer />
+      </Suspense>
     </div>
   );
 }
 
 export default App;
+
+
