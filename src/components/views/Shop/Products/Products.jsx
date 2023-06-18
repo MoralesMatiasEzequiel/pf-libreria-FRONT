f79b6eb69d0aa5c5728d80e31211337e3cc610b2
Lucas Rodriguez <94883797+Luckem67@users.noreply.github.com>
2023-06-18 10:03:59 -0300

    product

--- src/components/views/Shop/Products/Products.jsx
+++ src/components/views/Shop/Products/Products.jsx
@@ -1,16 +1,14 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { getProducts } from "../../../../redux/productActions";
-import {useDispatch, useSelector} from 'react-redux';
-import Detail from "../Detail/Detail";
-import {Link} from "react-router-dom";
-
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
 
 const Products = () => {
-	const dispatch = useDispatch();
-	const {products} = useSelector( state=> state.products)
-	useEffect(() => {
-		dispatch(getProducts());
-	},[dispatch])
+  const dispatch = useDispatch();
+  const { products } = useSelector((state) => state.products);
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
 
   return (
     <div className="container">
@@ -18,14 +16,24 @@ const Products = () => {
         {products.map((base, index) => {
           return (
             <div key={index} className="col-md-4">
-							<div className="card" >
-              <Link to={'/shop/' + base.id}>
-                <img className="card-img-top" src={base.image} alt="Card image cap"/>
-                <div className="card-body">
-									<h5 className="card-title">{base.name}</h5>
-                	<p className="card-text text-secondary">$ {base.price}</p>
-								</div>
-							</Link>
+              <div className="card">
+                <div class="card-body">
+                  <Link to={"/shop/" + base._id}>
+                    <h5 className="card-title">{base.name}</h5>
+                    <img
+                      className="card-img-top"
+                      src={base.image}
+                      alt="Card image cap"
+                    />
+                    <p className="card-text text-secondary">$ {base.price}</p>
+                  </Link>
+                </div>
+                <a href="#" class="btn btn-primary">
+                  <i className="bi bi-cart"></i>
+                </a>
+                <a href="#" class="btn btn-primary">
+                  <i className="bi bi-heart"></i>
+                </a>
               </div>
             </div>
           );
@@ -36,3 +44,16 @@ const Products = () => {
 };
 
 export default Products;
+
+{
+  /* <Link to={'/shop/' + base.id}>
+                <img className="card-img-top" src={base.image} alt="Card image cap"/>
+                <div className="card-body">
+									<h5 className="card-title">{base.name}</h5>
+                	<p className="card-text text-secondary">$ {base.price}</p>
+								</div>
+							</Link>
+							<div className="card-body ">
+
+							</div> */
+}
