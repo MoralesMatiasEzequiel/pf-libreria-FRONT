4239845254d71da8ac59fdbe3e306d3e6b09c9bd
Matías Morales <programacionfrida@gmail.com>
2023-06-19 15:55:13 -0300

    Componente Cart

--- src/components/views/Cart/Cart.jsx
+++ src/components/views/Cart/Cart.jsx
@@ -1,11 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import style from "./Cart.module.css";
+import { Link } from "react-router-dom";
+import FormInfoCart from "./FormInfoCart";
+import FormShipmentCart from "./FormShipmentCart";
+import FormPayCart from "./FormPayCart";
 
 const Cart = () => {
 
+    const [ formInfo, setFormInfo ] = useState(true);
+    const [ formShipment, setFormShipment ] = useState(false);
+    const [ formPay, setFormPay ] = useState(false);
 
     return (
-        <div>
-            <p>Soy el carrito</p>       
+        <div className={style.container}>
+            <div className={style.containerInfo}>
+                <Link to={"/shop"} className={style.linkBack}>{"< Volver a la tienda"}</Link> 
+                <h5 className={style.title}>Tu librito - Librería & Papelería</h5>   
+                <button className={style.boton} onClick={() => {setFormInfo(true); setFormShipment(false); setFormPay(false);}} disabled={formInfo}>Cliente</button>
+                <button className={style.boton} onClick={() => {setFormInfo(false); setFormShipment(true); setFormPay(false);}} disabled={formShipment}>Envío</button>
+                <button className={style.boton} onClick={() => {setFormInfo(false); setFormShipment(false); setFormPay(true);}} disabled={formPay}>Pago</button>
+                <FormInfoCart state={formInfo} setState={setFormInfo}/>
+                <FormShipmentCart state={formShipment} setState={setFormShipment}/>
+                <FormPayCart state={formPay} setState={setFormPay}/>
+            </div>   
+            <div className={style.containerSubTotal}>
+                <h5>Resumen de la compra</h5> 
+            </div>
         </div>
     )
 
