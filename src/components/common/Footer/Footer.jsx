87fe4ef404855fe34d27f551d785647638617acc
GJRC23 <gastuconsoli8@gmail.com>
2023-06-21 16:24:32 -0300

    Footer y Contacto más chico

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Footer = () => {
-
   const localAddress = "Dirección del local"; // Inserta aquí la dirección del local
 
   const openGoogleMaps = () => {
@@ -12,12 +11,12 @@ const Footer = () => {
   };
 
   return (
-    <footer className="bg-dark text-light py-4 mt-auto text-start">
+      <footer className="bg-dark text-light py-2 text-start" style={{ marginTop:"auto", bottom:"0", width:"100%"}}>
       <div className="container">
-        <div className="row">
+        <div className="row align-items-center">
           <div className="col-md-4">
-            <p>Correo electrónico: info@example.com</p>
-            <p>Teléfono: 123-456-7890</p>
+            <p className="mb-0 small">Correo electrónico: info@example.com</p>
+            <p className="mb-0 small">Teléfono: 123-456-7890</p>
             <ul className="list-inline">
               <li className="list-inline-item">
                 <a href="enlace-a-red-social-1" className="text-decoration-none">
@@ -27,35 +26,29 @@ const Footer = () => {
               <li className="list-inline-item">
                 <a href="enlace-a-red-social-2" className="text-decoration-none">
                   <i className="bi bi-instagram text-purple cursor-pointer" style={{ color: "purple" }}>
-                    {" "}
                     Instagram
                   </i>
                 </a>
               </li>
               <li className="list-inline-item">
                 <i className="bi bi-geo-alt text-success" style={{ cursor: "pointer" }} onClick={openGoogleMaps}>
-                  {" "}
                   Google Maps
                 </i>
               </li>
             </ul>
           </div>
-          <div className="col-md-4">
+          <div className="col-md-4 text-center">
             <Link to="/terminos" className="text-light text-decoration-none">
-              <button className="btn btn-outline-light">Ver términos y condiciones</button>
+              <button className="btn btn-outline-light btn-sm">Ver términos y condiciones</button>
             </Link>
-          </div>
-          <div className="col-md-4">
             <Link to="/arrepentimiento" className="text-decoration-none">
-              <button className="btn btn-outline-light">
+              <button className="btn btn-outline-light btn-sm ms-2">
                 Arrepentirse de la compra
               </button>
             </Link>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-md-12 text-center">
-            <p>&copy; {new Date().getFullYear()} Tu Librito</p>
+          <div className="col-md-4 text-end">
+            <p className="mb-0 small">&copy; {new Date().getFullYear()} Tu Librito</p>
           </div>
         </div>
       </div>
