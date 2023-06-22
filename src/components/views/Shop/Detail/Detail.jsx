df3117c90e8621ae4ca256524eb9d91b08545043
GJRC23 <gastuconsoli8@gmail.com>
2023-06-22 03:10:10 -0300

    Detail renovado

--- src/components/views/Shop/Detail/Detail.jsx
+++ src/components/views/Shop/Detail/Detail.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from "react-router-dom";
 import ModalCart from "../../../common/Modals/ModalCart/ModalCart";
+import style from "./Detail.module.css";
 
 const Detail = () => {
   const { id } = useParams();
@@ -19,35 +20,40 @@ const Detail = () => {
     return <div>Producto no encontrado</div>;
   }
 
+  const handleFavoriteClick = () => {
+    // Lógica para agregar o quitar de favoritos
+  };
+
   return (
     <div className="container">
       <div className="row">
-        <div className="col-md-6">
-          <div className="card">
-            <img className="card-img-top" src={product.image} alt={product.name} />
-            <div className="card-body">
-              <h5 className="card-title">{product.name}</h5>
-            </div>
+        <div className="col-md-6 d-flex align-items-center">
+          <div className="card" style={{ width: "100%", height: "450px", display: "flex", justifyContent: "center", alignItems: "center", marginTop: "25px", marginBottom: "25px" }}>
+            <img className="card-img-top" src={product.image} alt={product.name} style={{ maxWidth: "300px", maxHeight: "400px" }} />
           </div>
         </div>
-        <div className="col-md-6">
-          <p>Marca: {product.brand}</p>
-          <p>Stock: {product.stock}</p>
-          <p>Rating: {product.rating}</p>
-          <p>Detalle del producto: {product.description}</p>
-          <p>Precio: ${product.price}</p>
-          <button className="btn btn-primary" onClick={() => setModalShow(true)}>
-            <i className="bi bi-cart"></i> Comprar
-          </button>
-          <button className="btn btn-outline-danger">
-            <i className="bi bi-heart"></i> Favoritos
-          </button>
+        <div className="col-md-6 d-flex align-items-center justify-content-center">
+          <div className="card" style={{ width: "100%", height: "450px", marginTop: "25px", marginBottom: "25px" }}>
+          <div className="card-body" style={{ padding: "10px" }}>
+            <h5 className="card-title" style={{ color: "#191919", fontFamily: "Montserrat, sans-serif", fontWeight: "bold", fontSize: "24px", padding: "10px" }}>{product.name}</h5>
+            <p style={{ color: "#3F3F3F", fontSize: "18px", padding: "10px" }}>Marca: {product.brand}</p>
+            <p style={{ color: "#3F3F3F", fontSize: "18px", padding: "10px" }}>Stock: {product.stock}</p>
+            <p style={{ color: "#3F3F3F", fontSize: "18px", padding: "10px" }}>Rating: {product.rating}</p>
+            <p style={{ color: "#3F3F3F", fontSize: "18px", padding: "10px" }}>Detalle del producto: {product.description}</p>
+            <h5 style={{ color: "#191919", fontFamily: "Montserrat, sans-serif", fontWeight: "bold", fontSize: "24px", padding: "10px" }}>$ {product.price}</h5>
+            <button className={style.btnPrimary}  onClick={() => setModalShow(true)}>Comprar</button>
+            <button className={style.btnFavorite}  onClick={handleFavoriteClick}>
+              <i className="bi bi-heart"></i> Favoritos
+            </button>
+          </div>
+
+          </div>
         </div>
       </div>
-    <ModalCart 
-    show={modalShow}
-    onHide={() => setModalShow(false)}
-    />
+      <ModalCart
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
     </div>
   );
 };
