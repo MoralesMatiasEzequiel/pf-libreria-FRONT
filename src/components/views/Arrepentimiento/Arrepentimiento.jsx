cf4a5e2967a27855055d0199142ffdee6a9f78bd
GJRC23 <gastuconsoli8@gmail.com>
2023-06-22 01:53:50 -0300

    Logo Nuevo y etc

--- src/components/views/Arrepentimiento/Arrepentimiento.jsx
+++ src/components/views/Arrepentimiento/Arrepentimiento.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import style from "./Arrepentimiento.module.css";
 
 const Arrepentimiento = () => {
   const [nombre, setNombre] = useState("");
@@ -55,8 +56,8 @@ const Arrepentimiento = () => {
 
   return (
     <div className="container">
-      <h1>Arrepentimiento de la compra</h1>
-      <p>
+      <h1 style={{ color: "#FF9E5C", fontWeight: "bold", fontSize: "24px" }}>Arrepentimiento de la compra</h1>
+      <p style={{ color: "#3F3F3F", fontSize: "18px" }}>
         Si deseas arrepentirte de tu compra, por favor completa el siguiente formulario y nos pondremos en contacto contigo.
       </p>
 
@@ -64,29 +65,81 @@ const Arrepentimiento = () => {
         <div className="row">
           <div className="col-md-6">
             <div className="mb-3">
-              <input type="text" id="nombre" name="nombre" className="form-control" placeholder="Nombre" value={nombre} onChange={(e) => setNombre(e.target.value)} required />
+              <input
+                type="text"
+                id="nombre"
+                name="nombre"
+                className="form-control"
+                placeholder="Nombre"
+                value={nombre}
+                onChange={(e) => setNombre(e.target.value)}
+                required
+              />
             </div>
 
             <div className="mb-3">
-              <input type="text" id="apellido" name="apellido" className="form-control" placeholder="Apellido" value={apellido} onChange={(e) => setApellido(e.target.value)} required />
+              <input
+                type="text"
+                id="apellido"
+                name="apellido"
+                className="form-control"
+                placeholder="Apellido"
+                value={apellido}
+                onChange={(e) => setApellido(e.target.value)}
+                required
+              />
             </div>
 
             <div className="mb-3">
-              <input type="email" id="email" name="email" className="form-control" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="form-control"
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
             </div>
 
             <div className="mb-3">
-              <input type="tel" id="telefono" name="telefono" className="form-control" placeholder="Teléfono" value={telefono} onChange={(e) => setTelefono(e.target.value)} required />
+              <input
+                type="tel"
+                id="telefono"
+                name="telefono"
+                className="form-control"
+                placeholder="Teléfono"
+                value={telefono}
+                onChange={(e) => setTelefono(e.target.value)}
+                required
+              />
             </div>
 
             <div className="mb-3">
-              <input type="text" id="numeroDocumento" name="numeroDocumento" className="form-control" placeholder="Número de documento" value={numeroDocumento} onChange={(e) => setNumeroDocumento(e.target.value)} required />
+              <input
+                type="text"
+                id="numeroDocumento"
+                name="numeroDocumento"
+                className="form-control"
+                placeholder="Número de documento"
+                value={numeroDocumento}
+                onChange={(e) => setNumeroDocumento(e.target.value)}
+                required
+              />
             </div>
 
             <div className="row">
               <div className="col-md-6">
                 <div className="mb-3">
-                  <select id="modoDevolucion" name="modoDevolucion" className="form-select" value={modoDevolucion} onChange={(e) => setModoDevolucion(e.target.value)} required>
+                  <select
+                    id="modoDevolucion"
+                    name="modoDevolucion"
+                    className="form-select"
+                    value={modoDevolucion}
+                    onChange={(e) => setModoDevolucion(e.target.value)}
+                    required
+                  >
                     <option value="">Seleccionar modo de devolución</option>
                     <option value="Correo">Correo</option>
                     <option value="Retiro en domicilio">Retiro en domicilio</option>
@@ -96,7 +149,14 @@ const Arrepentimiento = () => {
 
               <div className="col-md-6">
                 <div className="mb-3">
-                  <select id="motivoDevolucion" name="motivoDevolucion" className="form-select" value={motivoDevolucion} onChange={(e) => setMotivoDevolucion(e.target.value)} required>
+                  <select
+                    id="motivoDevolucion"
+                    name="motivoDevolucion"
+                    className="form-select"
+                    value={motivoDevolucion}
+                    onChange={(e) => setMotivoDevolucion(e.target.value)}
+                    required
+                  >
                     <option value="">Seleccionar motivo de devolución</option>
                     <option value="Producto defectuoso">Producto defectuoso</option>
                     <option value="Producto incorrecto">Producto incorrecto</option>
@@ -107,53 +167,137 @@ const Arrepentimiento = () => {
             </div>
 
             <div className="mb-3">
-              <label htmlFor="recibido" className="form-label">¿Recibiste tu pedido?</label>
+              <label htmlFor="recibido" className="form-label" style={{ color: "#3F3F3F", fontWeight: "bold" }}>
+                ¿Recibiste tu pedido?
+              </label>
               <div className="form-check form-check-inline">
-                <input type="radio" id="recibido-yes" name="recibido" className="form-check-input" value="Sí" checked={recibido} onChange={() => setRecibido(true)} required />
-                <label htmlFor="recibido-yes" className="form-check-label">Sí</label>
+                <input
+                  type="radio"
+                  id="recibido-yes"
+                  name="recibido"
+                  className="form-check-input"
+                  value="Sí"
+                  checked={recibido}
+                  onChange={() => setRecibido(true)}
+                  required
+                />
+                <label htmlFor="recibido-yes" className="form-check-label" style={{ color: "#3F3F3F" }}>
+                  Sí
+                </label>
               </div>
               <div className="form-check form-check-inline">
-                <input type="radio" id="recibido-no" name="recibido" className="form-check-input" value="No" checked={!recibido} onChange={() => setRecibido(false)} required />
-                <label htmlFor="recibido-no" className="form-check-label">No</label>
+                <input
+                  type="radio"
+                  id="recibido-no"
+                  name="recibido"
+                  className="form-check-input"
+                  value="No"
+                  checked={!recibido}
+                  onChange={() => setRecibido(false)}
+                  required
+                />
+                <label htmlFor="recibido-no" className="form-check-label" style={{ color: "#3F3F3F" }}>
+                  No
+                </label>
               </div>
             </div>
           </div>
 
           <div className="col-md-6">
-
             <div className="mb-3">
-              <input type="text" id="direccion" name="direccion" className="form-control" placeholder="Dirección" value={direccion} onChange={(e) => setDireccion(e.target.value)} required />
+              <input
+                type="text"
+                id="direccion"
+                name="direccion"
+                className="form-control"
+                placeholder="Dirección"
+                value={direccion}
+                onChange={(e) => setDireccion(e.target.value)}
+                required
+              />
             </div>
 
             <div className="mb-3">
-              <input type="text" id="piso" name="piso" className="form-control" placeholder="Piso" value={piso} onChange={(e) => setPiso(e.target.value)} />
+              <input
+                type="text"
+                id="piso"
+                name="piso"
+                className="form-control"
+                placeholder="Piso"
+                value={piso}
+                onChange={(e) => setPiso(e.target.value)}
+              />
             </div>
 
             <div className="mb-3">
-              <input type="text" id="departamento" name="departamento" className="form-control" placeholder="Departamento" value={departamento} onChange={(e) => setDepartamento(e.target.value)} />
+              <input
+                type="text"
+                id="departamento"
+                name="departamento"
+                className="form-control"
+                placeholder="Departamento"
+                value={departamento}
+                onChange={(e) => setDepartamento(e.target.value)}
+              />
             </div>
 
             <div className="mb-3">
-              <input type="text" id="codigoPostal" name="codigoPostal" className="form-control" placeholder="Código Postal" value={codigoPostal} onChange={(e) => setCodigoPostal(e.target.value)} required />
+              <input
+                type="text"
+                id="codigoPostal"
+                name="codigoPostal"
+                className="form-control"
+                placeholder="Código Postal"
+                value={codigoPostal}
+                onChange={(e) => setCodigoPostal(e.target.value)}
+                required
+              />
             </div>
 
             <div className="mb-3">
-              <input type="text" id="localidad" name="localidad" className="form-control" placeholder="Localidad" value={localidad} onChange={(e) => setLocalidad(e.target.value)} required />
+              <input
+                type="text"
+                id="localidad"
+                name="localidad"
+                className="form-control"
+                placeholder="Localidad"
+                value={localidad}
+                onChange={(e) => setLocalidad(e.target.value)}
+                required
+              />
             </div>
 
             <div className="mb-3">
-              <input id="provincia" name="provincia" className="form-control" placeholder="Provincia" value={provincia} onChange={(e) => setProvincia(e.target.value)} required />
+              <input
+                id="provincia"
+                name="provincia"
+                className="form-control"
+                placeholder="Provincia"
+                value={provincia}
+                onChange={(e) => setProvincia(e.target.value)}
+                required
+              />
             </div>
           </div>
-
         </div>
 
         <div className="mb-3">
-          <textarea id="comentario" name="comentario" placeholder="Comentario" className="form-control" rows="1" value={comentario} onChange={(e) => setComentario(e.target.value)} required></textarea>
+          <textarea
+            id="comentario"
+            name="comentario"
+            placeholder="Comentario"
+            className="form-control"
+            rows="1"
+            value={comentario}
+            onChange={(e) => setComentario(e.target.value)}
+            required
+          ></textarea>
         </div>
 
-        <div className="text-center" style={{paddingBottom: "10px"}}>
-          <button type="submit" className="btn btn-primary">Enviar</button>
+        <div className="text-center" style={{ paddingBottom: "10px" }}>
+          <button type="submit" className= {style.btnPrimary} >
+            Enviar
+          </button>
         </div>
       </form>
     </div>
