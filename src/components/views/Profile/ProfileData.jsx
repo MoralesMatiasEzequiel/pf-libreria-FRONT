111307c3f78ce551b2d372ada9674ac96630a77d
Sofía Chiabudini <sofiachiabudini@gmail.com>
2023-06-27 21:52:05 -0300

    currentUser viene por back / arreglé un error en profileData

--- src/components/views/Profile/ProfileData.jsx
+++ src/components/views/Profile/ProfileData.jsx
@@ -5,22 +5,23 @@ import { useAuth0 } from "@auth0/auth0-react";
 const ProfileData = () => {
 
     const { currentUser } = useSelector(state => state.user);
-    // const { user } = useAuth0();
-    // let imgLink = '';
-    // console.log(user.sub);
+    const { user } = useAuth0();
+    let imgLink = '';
 
-    // if(user.sub.includes('google')){
-    //     [imgLink] = currentUser.picture.split('=');
-    // } else {
-    //     imgLink = currentUser.picture; 
-    // }
+    console.log(currentUser);
+
+    if(user.sub.includes('google')){
+        [imgLink] = currentUser.picture.split('=');
+    } else {
+        imgLink = currentUser.picture; 
+    }
 
     return (
         <div className={styles.view}>
             <h1 className={styles.title}>DATOS</h1>
             <div className={styles.container}>
                 <div className={styles["image-container"]}>
-                    <img src={currentUser.picture} alt={currentUser.name} />
+                    <img src={imgLink} alt={currentUser.name} />
                 </div>
                 <div className={styles["data-container"]}>
                     <p>Usuario: {currentUser.name}</p>
