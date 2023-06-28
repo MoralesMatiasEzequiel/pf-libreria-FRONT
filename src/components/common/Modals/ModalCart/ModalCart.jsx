91fb7a8671da151f16f36b7bd71a651a8fb47c67
GJRC23 <gastuconsoli8@gmail.com>
2023-06-28 03:09:58 -0300

    Modal de Compra

--- src/components/common/Modals/ModalCart/ModalCart.jsx
+++ src/components/common/Modals/ModalCart/ModalCart.jsx
@@ -3,14 +3,26 @@ import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import { Link, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
-
+import { useState } from "react";
 
 const ModalCart = (props) => {
+  const { id } = useParams();
+  const { products } = useSelector(state => state.products);
+  const product = products.find(item => item._id === id);
 
-    const { id } = useParams();
-    const { products } = useSelector(state => state.products);
-    const product = products.find(item => item._id === id);
+  const [selectedStock, setSelectedStock] = useState(1); // Estado para almacenar la cantidad seleccionada
 
+  const handleStockChangeModal = (change) => {
+    const newStock = selectedStock + change;
+    const productStock = product.stock || 1;
+
+    if (newStock > 0 && newStock <= productStock) {
+      setSelectedStock(newStock);
+    }
+  };
+
+  // Calcula el precio total multiplicando el precio del producto por la cantidad seleccionada
+  const total = product.price * selectedStock;
 
   return (
     <Modal
@@ -26,31 +38,33 @@ const ModalCart = (props) => {
       </Modal.Header>
       <Modal.Body className={style.bodyModal}>
         <div className={style.containerBody}>
-            <h6 className={style.titleProduct}>Producto</h6>
-            <h6 className={style.titleStock}>Cantidad</h6>
-            <h6 className={style.titlePrice}>Total</h6>
-        </div> 
-        <div className={style.containerBody}>
-        <img className={style.imagen} src={product.image} alt={product.name} />
-        <p className={style.name}>{product.name}</p>
-        {/* <Link to={`/shop/${id}`}>{product.name}</Link> */}
-        <select name="stock" id="" className={style.stock}>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-            <option value="6">6</option>
-            <option value="7">7</option>
-            <option value="8">8</option>
-            <option value="9">9</option>
-            <option value="10">10</option>
-        </select>
-        <p>${product.price}</p>
+          <img className={style.imagen} src={product.image} alt={product.name} />
+          <p className={style.name}>{product.name}</p>
+          <div className={style.quantityContainer}>
+            <p className={style.quantityLabel}>Cantidad:</p>
+            <div className={style.stock}>
+              <button
+                className={style.quantityButton}
+                onClick={() => handleStockChangeModal(-1)}
+                disabled={selectedStock === 1}
+              >
+                -
+              </button>
+              <span className={style.quantityValue}>{selectedStock}</span>
+              <button
+                className={style.quantityButton}
+                onClick={() => handleStockChangeModal(1)}
+                disabled={selectedStock === (product.stock || 1)}
+              >
+                +
+              </button>
+            </div>
+          </div>
+          <p className={style.totalPrice}>Total: ${total}</p> {/* Muestra el precio total */}
         </div>
       </Modal.Body>
       <Modal.Footer>
-        <Button onClick={props.onHide}>Seguir comprando</Button>
+        <Button onClick={props.onHide} className={style.seguir}>Seguir comprando</Button>
         <Link to={"/cart"} className={style.finish}>Finalizar compra</Link>
       </Modal.Footer>
     </Modal>
