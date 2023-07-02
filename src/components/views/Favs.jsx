58fefce8f4fd8f6efa02e4cb94cfc0c5f77a4b9a
Lucas Rodriguez <94883797+Luckem67@users.noreply.github.com>
2023-07-02 07:08:08 -0300

    Update Favs.jsx

--- src/components/views/Favs.jsx
+++ src/components/views/Favs.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-// import { addCart } from '../features/carts/cartsSlice';
 import { removeFavorite, clearFav } from '../../redux/favoriteActions';
 
 
@@ -14,21 +13,11 @@ const Favs = ({ favorites }) => {
     const dispatch = useDispatch();
   
 
-    // remove wish item
+    // remove favs item
     const removeFavsHandler = (productId) => {
 			dispatch(removeFavorite(productId));
     }
 
-		// const clearFavoritesHandler = (productId) => {
-		// 	dispatch(clearFav(productId));
-		// };
-
-
-    // // add to cart in wish page
-    // const addToCartHandler = (product) => {
-    //     dispatch(addCart({ product, qty: Number(1) }));
-    //     navigate('/carts');
-    // }
 
 
 
@@ -42,7 +31,6 @@ const Favs = ({ favorites }) => {
                 <h3 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h3>
                 <h4 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">$ {price}</h4>
                 <div className='flex flex-row  items-center py-1'>
-                    {/* <button onClick={() => addToCartHandler(wishlist)} className='text-white bg-rose-600 hover:bg-rose-800 rounded-lg mx-2 px-2 md:whitespace-pre'>Add To Cart</button> */}
                     <button onClick={() => removeFavsHandler(favorites)} className='dark:text-rose-600'>
 										<i class="bi bi-trash"></i>
                     </button>
