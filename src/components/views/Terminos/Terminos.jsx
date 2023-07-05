9a592db6887e15ab8f9baaaeefc4dddeb2001ca6
GJRC23 <gastuconsoli8@gmail.com>
2023-07-05 03:13:30 -0300

    imagen de feature en desarrollo

--- src/components/views/Terminos/Terminos.jsx
+++ src/components/views/Terminos/Terminos.jsx
@@ -1,12 +1,13 @@
 import styles from './Terminos.module.css';
+import WorkingImage from './Working.png';
 
 const Terminos = () => {
 
 
     return (
         <div className={styles.container}>
-            <h1 className={styles.title}>FEATURE EN DESARROLLO</h1>
-            <img className={styles.imgDev} src="https://www.pngkey.com/png/detail/966-9667187_how-we-work-entrada-de-trabajo-dibujo.png" alt="" />
+            <h1 className={styles.title}>FEATURE A DESARROLLAR</h1>
+            <img className={styles.imgDev} src={WorkingImage} alt="" />
         </div>
       );
 }
